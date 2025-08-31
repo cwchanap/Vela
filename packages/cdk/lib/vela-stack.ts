ab85b7ff8749640b7f7873941e1343d17701bb37
cwchanap <cwchanap@connect.ust.hk>
2025-08-31 12:32:14 -0700

    feat: refactor VelaStack to use updated AWS CDK imports and remove deprecated scripts

--- packages/cdk/lib/vela-stack.ts
+++ packages/cdk/lib/vela-stack.ts
@@ -1,35 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import type { Construct } from 'constructs';
+import { Stack, StackProps, Duration, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { RestApi, LambdaIntegration, Cors } from 'aws-cdk-lib/aws-apigateway';
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import {
+  Distribution,
+  ViewerProtocolPolicy,
+  CachePolicy,
+  AllowedMethods,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin, RestApiOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Construct } from 'constructs';
 import * as path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// CommonJS globals are available in this context
+declare const __dirname: string;
+declare const process: any;
 
-export class VelaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class VelaStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // DynamoDB Table for Chat History
-    const chatHistoryTable = new dynamodb.Table(this, 'VelaTable', {
+    const chatHistoryTable = new Table(this, 'VelaTable', {
       tableName: 'vela',
       partitionKey: {
         name: 'ThreadId',
-        type: dynamodb.AttributeType.STRING,
+        type: AttributeType.STRING,
       },
       sortKey: {
         name: 'Timestamp',
-        type: dynamodb.AttributeType.NUMBER,
+        type: AttributeType.NUMBER,
       },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
       pointInTimeRecoverySpecification: {
         pointInTimeRecoveryEnabled: true,
       },
@@ -40,21 +45,21 @@ export class VelaStack extends cdk.Stack {
       indexName: 'UserIdIndex',
       partitionKey: {
         name: 'UserId',
-        type: dynamodb.AttributeType.STRING,
+        type: AttributeType.STRING,
       },
       sortKey: {
         name: 'Timestamp',
-        type: dynamodb.AttributeType.NUMBER,
+        type: AttributeType.NUMBER,
       },
     });
 
     // Lambda Function
-    const apiLambda = new lambda.Function(this, 'VelaApiFunction', {
+    const apiLambda = new Function(this, 'VelaApiFunction', {
       functionName: 'vela-api',
-      runtime: lambda.Runtime.NODEJS_20_X,
+      runtime: Runtime.NODEJS_20_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../dist/lambda')),
-      timeout: cdk.Duration.seconds(30),
+      code: Code.fromAsset(path.join(__dirname, '../dist/lambda')),
+      timeout: Duration.seconds(30),
       memorySize: 512,
       environment: {
         DYNAMODB_TABLE_NAME: chatHistoryTable.tableName,
@@ -67,12 +72,12 @@ export class VelaStack extends cdk.Stack {
     chatHistoryTable.grantReadWriteData(apiLambda);
 
     // API Gateway
-    const api = new apigateway.RestApi(this, 'VelaApi', {
+    const api = new RestApi(this, 'VelaApi', {
       restApiName: 'Vela API',
       description: 'API for Vela Japanese Learning App',
       defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
-        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
         allowHeaders: [
           'Content-Type',
           'X-Amz-Date',
@@ -84,7 +89,7 @@ export class VelaStack extends cdk.Stack {
     });
 
     // API Gateway Integration
-    const lambdaIntegration = new apigateway.LambdaIntegration(apiLambda);
+    const lambdaIntegration = new LambdaIntegration(apiLambda);
 
     // Add API routes
     const apiResource = api.root.addResource('api');
@@ -94,29 +99,29 @@ export class VelaStack extends cdk.Stack {
     });
 
     // S3 Bucket for Static Website
-    const websiteBucket = new s3.Bucket(this, 'VelaWebBucket', {
+    const websiteBucket = new Bucket(this, 'VelaWebBucket', {
       bucketName: 'vela-web',
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'index.html',
       publicReadAccess: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      removalPolicy: RemovalPolicy.RETAIN,
       autoDeleteObjects: false,
     });
 
     // CloudFront Distribution
-    const distribution = new cloudfront.Distribution(this, 'VelaDistribution', {
+    const distribution = new Distribution(this, 'VelaDistribution', {
       defaultBehavior: {
-        origin: new origins.S3Origin(websiteBucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        origin: new S3Origin(websiteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: CachePolicy.CACHING_OPTIMIZED,
       },
       additionalBehaviors: {
         '/api/*': {
-          origin: new origins.RestApiOrigin(api),
-          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          origin: new RestApiOrigin(api),
+          viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: CachePolicy.CACHING_DISABLED,
+          allowedMethods: AllowedMethods.ALLOW_ALL,
         },
       },
       errorResponses: [
@@ -134,30 +139,30 @@ export class VelaStack extends cdk.Stack {
     });
 
     // Deploy website to S3
-    new s3deploy.BucketDeployment(this, 'VelaWebsiteDeployment', {
-      sources: [s3deploy.Source.asset(path.join(__dirname, '../../../apps/vela/dist/spa'))],
+    new BucketDeployment(this, 'VelaWebsiteDeployment', {
+      sources: [Source.asset(path.join(__dirname, '../../../apps/vela/dist/spa'))],
       destinationBucket: websiteBucket,
       distribution,
       distributionPaths: ['/*'],
     });
 
     // Outputs
-    new cdk.CfnOutput(this, 'WebsiteURL', {
+    new CfnOutput(this, 'WebsiteURL', {
       value: `https://${distribution.distributionDomainName}`,
       description: 'Website URL',
     });
 
-    new cdk.CfnOutput(this, 'ApiURL', {
+    new CfnOutput(this, 'ApiURL', {
       value: api.url,
       description: 'API Gateway URL',
     });
 
-    new cdk.CfnOutput(this, 'DynamoDBTableName', {
+    new CfnOutput(this, 'DynamoDBTableName', {
       value: chatHistoryTable.tableName,
       description: 'DynamoDB Table Name',
     });
 
-    new cdk.CfnOutput(this, 'CloudFrontDistributionId', {
+    new CfnOutput(this, 'CloudFrontDistributionId', {
       value: distribution.distributionId,
       description: 'CloudFront Distribution ID',
     });
