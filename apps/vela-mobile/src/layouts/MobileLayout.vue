0f7c232e74c1be412deadab81b08cd39e90e2b24
cwchanap <cwchanap@connect.ust.hk>
2026-07-14 22:14:30 -0700

    feat(vela-mobile): add MobileLayout with 5-tab bottom navigation

---
+++ apps/vela-mobile/src/layouts/MobileLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="mobile-nav">
+      <q-tabs dense no-caps align="justify" class="bg-white text-primary nav-tabs" :breakpoint="0">
+        <q-route-tab to="/" icon="home" label="Home" />
+        <q-route-tab to="/review" icon="repeat" label="Review" />
+        <q-route-tab to="/learn" icon="school" label="Learn" />
+        <q-route-tab to="/words" icon="menu_book" label="Words" />
+        <q-route-tab to="/more" icon="more_horiz" label="More" />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.mobile-nav {
+  padding-bottom: env(safe-area-inset-bottom, 0px);
+}
+</style>
